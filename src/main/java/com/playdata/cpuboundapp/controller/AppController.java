5b7f3577929790b1817e3ee1c8d4aeb87727c71c
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-02 16:55:55 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook tes t. ...";
+        return "webhook ...!!!! BYE ";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
